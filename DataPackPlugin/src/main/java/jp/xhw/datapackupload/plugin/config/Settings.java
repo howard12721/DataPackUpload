35ea68e4724b1c9fe0438b10ddd65fffcb986a74
howard127 <kobun127@gmail.com>
2023-09-23 08:20:10 +0900

    [update] add config

---
+++ DataPackPlugin/src/main/java/jp/xhw/datapackupload/plugin/config/Settings.java
@@ -0,0 +1,22 @@
+package jp.xhw.datapackupload.plugin.config;
+
+import lombok.Getter;
+import net.william278.annotaml.YamlFile;
+import net.william278.annotaml.YamlKey;
+
+import java.util.List;
+
+@Getter
+@YamlFile
+public class Settings {
+
+    @YamlKey("discord.token")
+    private String botToken;
+
+    @YamlKey("discord.upload-channels")
+    private List<Long> uploadChannels;
+
+    @YamlKey("discord.upload-roles")
+    private List<Long> uploadRoles;
+
+}
